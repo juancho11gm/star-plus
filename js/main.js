3f3b20e591f2e1e10b2c23c40235ec88bf7f91b0
juancho11gm <juansebastian11gm@gmail.com>
2022-04-20 00:16:21 -0500

    feat(component): add accordion component
    feat #12

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('Hello Star +');
+const accordionCollection = document.querySelectorAll(
+  '.accordion-container__summary',
+);
+
+accordionCollection.forEach((accordionSummary) => {
+  // Smooth accordion animation: https://stackoverflow.com/questions/3508605/how-can-i-transition-height-0-to-height-auto-using-css
+  accordionSummary.addEventListener('click', (e) => {
+    accordionSummary.classList.toggle('open');
+    // .accordion-container__content
+    const accordionContent = e.target.nextElementSibling;
+    // .accordion-container__description
+    const [accordionDescription] = accordionContent.children;
+    const { clientHeight: descriptionHeight } = accordionDescription;
+    accordionContent.style.height = accordionContent.clientHeight
+      ? 0
+      : `${descriptionHeight}px`;
+  });
+});
