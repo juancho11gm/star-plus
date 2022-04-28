5c8ff49271741e3ce8e0d45e3cfb4949bca2feee
Jaimeandres97 <ja.moreno12@uniandes.edu.co>
2022-04-28 16:52:39 -0500

    refactor: split architecture by components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import './styles/main.scss';
-
+import './components/Header/header.scss';
+import './components/Hero/hero.scss';
+import './components/Plans/plans.scss';
 import './components/Accordion/Accordion';
 import './components/Footer/Footer';
