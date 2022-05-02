b3a12874977a71b71985aec9ccf1422f68ead67d
Juan Sebastián <juansebastian11gm@gmail.com>
2022-05-01 22:10:19 -0500

    refactor(architecture): split architecture by components (#18)
    includes scss with vite build tool
    
    BREAKING CHANGE: architecture refactor

--- js/main.js
+++
@@ -1,5 +0,0 @@
-import AccordionView from './views/accordion.js';
-
-const accordionCollection = document.querySelectorAll('.accordion-container');
-const accordionView = new AccordionView(accordionCollection);
-accordionView.addAccordionAnimation();
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './styles/main.scss';
+
+import './components/Accordion/Accordion';
+import './components/Footer/Footer';
