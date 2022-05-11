0235eb231e43e849a78fe37ab94a9c6905657633
Jaimeandres97 <35746531+Jaimeandres97@users.noreply.github.com>
2022-05-10 20:54:29 -0500

    Feat/#13 devices component (#19)
    * refactor: split architecture by components
    
    * style: capitilize .scss files in components
    
    * feat: #13 add devices component in the components strategy
    
    * feat: #13 devices component: add figure and figcaption elements to HTML. SCSS variables
    
    correspondent folders
    
    * style: #13 devices component: Plans.scss selectors in .plan code block
    
    * style: #13 devices component: change folder name img to public
    
    * style: #13 devices component: update images references

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import './styles/main.scss';
-
+import './components/Header/Header.scss';
+import './components/Hero/Hero.scss';
+import './components/Plans/Plans.scss';
+import './components/Devices/Devices.scss';
 import './components/Accordion/Accordion';
 import './components/Footer/Footer';
